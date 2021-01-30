afa8b4630d9b02750e571057ef4354711f1b675e
Andrey1303110 <andrey1303110@gmail.com>
2021-01-30 18:45:43 +0200

    section 4 done adapt

--- js/slider.js
+++ js/slider.js
@@ -12,15 +12,34 @@ $(document).ready(function () {
         verticalSwiping: true,
     });
 });
+
 $(document).ready(function () {
-    $('.slider-news').slick({
-        arrows: true,
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        draggable: true,
-        initialSlide: 2,
-        speed: 950,
-    });
+    if (window.innerWidth < 854) {
+        $(document).ready(function () {
+            $('.slider-news').slick({
+                arrows: true,
+                dots: true,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                draggable: true,
+                initialSlide: 2,
+                speed: 650,
+            });
+        });
+    }
+    else {
+        $(document).ready(function () {
+            $('.slider-news').slick({
+                arrows: true,
+                dots: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                draggable: true,
+                initialSlide: 2,
+                speed: 950,
+            });
+        });
+    }
 });
